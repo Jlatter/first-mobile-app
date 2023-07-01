b7c4548f537067de03a17b67b27e054adb0f73ad
Jordan Latter <Jlatter1111@gmail.com>
2023-07-01 20:03:17 +0100

    create a basic student data app using kivy make a gui that takes 3 inputs for student, name, marks and age

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("color")
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class ChildApp(GridLayout):
+    def __init__(self, **kwargs):
+        super(ChildApp, self).__init__()
+        self.rows = 2
+        self.add_widget(Label(text='Student Name'))
+        self.s_name = TextInput()
+        self.add_widget(self.s_name)
+        self.add_widget(Label(text='Student Marks'))
+        self.s_marks = TextInput()
+        self.add_widget(self.s_marks)
+        self.add_widget(Label(text='Student Age'))
+        self.s_age = TextInput()
+        self.add_widget(self.s_age)
+
+
+class ParentApp(App):
+    def build(self):
+        return ChildApp()
+
+
+if __name__ == "__main__":
+    ParentApp().run()
